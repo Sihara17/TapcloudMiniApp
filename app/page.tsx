09742b4972d730d97f76690b30849697e14e718d
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-07-10 22:29:56 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
-"use client"
-
 import TapCloudClient from "@/components/TapCloudClient"
-import Link from "next/link"
-import { Home, Zap, Target } from "lucide-react"
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-blue light text-white flex flex-col items-center pb-24">
-      {/* TapCloud Game UI */}
+    <main>
       <TapCloudClient />
-
-      {/* Bottom Navigation */}
-      <nav className="fixed bottom-4 inset-x-0 flex justify-around px-6">
-        <Link href="/" className="text-white hover:text-cyan-400">
-          <Home size={32} />
-        </Link>
-        <Link href="/boost" className="text-white hover:text-cyan-400">
-          <Zap size={32} />
-        </Link>
-        <Link href="/quest" className="text-white hover:text-cyan-400">
-          <Target size={32} />
-        </Link>
-      </nav>
-    </div>
+    </main>
   )
 }
